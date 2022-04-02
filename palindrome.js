0073214a8d1fd7901fe00b8ea7ebae2460cfe78b
Yaak0v <jacobsandry@gmail.com>
2022-04-02 17:42:08 -0400

    prototypes

--- palindrome.js
+++ palindrome.js
@@ -10,11 +10,26 @@ function palindrome(string) {
 // Defines a Phrase object.
 function Phrase(content) {
     this.content = content;
+    this.processor = function(string) {
+        return string.toLowerCase();
+    }
+    this.processedContent = function processedContent() {
+        return this.processor(this.content);
+    }
     this.palindrome = function palindrome() {
-        let processedContent = this.content.toLowerCase();
-        return processedContent === reverse(processedContent);
+        return this.processedContent() === reverse(this.processedContent());
     }
     this.louder = function() {
         return this.content.toUpperCase();
     }
 }
+
+function TranslatedPhrase(content, translation) {
+    this.content = content;
+    this.translation = translation;
+    this.processedContent = function processedContent() {
+        return this.processor(this.content);
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase();
