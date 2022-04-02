53edfe7ba2499e84962802fd5ec7f6725677ae6e
Yaak0v <jacobsandry@gmail.com>
2022-04-01 20:42:11 -0400

    add constructor function for phrase in palindrome

--- palindrome.js
+++ palindrome.js
@@ -6,3 +6,15 @@ function palindrome(string) {
     let processedContent = string.toLowerCase();
     return processedContent === reverse(processedContent);
 }
+
+// Defines a Phrase object.
+function Phrase(content) {
+    this.content = content;
+    this.palindrome = function palindrome() {
+        let processedContent = this.content.toLowerCase();
+        return processedContent === reverse(processedContent);
+    }
+    this.louder = function() {
+        return this.content.toUpperCase();
+    }
+}
